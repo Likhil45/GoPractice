359523fcfd97c06a43034042ec3563f417246282
Likhil45 <likhilcharan.vellanki99@gmail.com>
2025-01-13 09:36:27 -0600

    Updated Logic

--- struct.go
+++ struct.go
@@ -6,6 +6,9 @@ func main() {
 	std1 := Student{"Likhil", []int{80, 90}}
 	std1.addMark(100)
 	std1.CalculateAverage()
+	fmt.Println(std1)
+	fmt.Printf("Average = %f", std1.CalculateAverage())
+
 }
 
 type Student struct {
@@ -13,11 +16,16 @@ type Student struct {
 	Marks []int
 }
 
-func (s Student) addMark(mark int) {
+func (s *Student) addMark(mark int) {
 	s.Marks = append(s.Marks, mark)
-	fmt.Println(s)
-}
-func (s Student) CalculateAverage() float64 {
 
-	return (s.CalculateAverage())
+}
+func (s *Student) CalculateAverage() float64 {
+	var tot float64
+	var avg float64
+	for _, val := range s.Marks {
+		tot = tot + float64(val)
+	}
+	avg = tot / float64((len(s.Marks)))
+	return avg
 }
